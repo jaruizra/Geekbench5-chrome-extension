dd47ddb256723ebf5c6fc37f2d43b1f28631afd5
jaruizra <121313957+jaruizra@users.noreply.github.com>
2023-06-01 22:08:50 +0200

    Delete try directory

--- try/script_web.js
+++
@@ -1,8 +0,0 @@
-import { spawn } from 'child_process';
-
-const page_url = window.location.href;
-const python_process = spawn('python', ['./geekbench5.py', page_url]);
-
-python_process.stdout.on('data', (confirmation) => {
-    console.log('okkkkk', confirmation.toString());
-});
